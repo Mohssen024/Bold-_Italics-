6c519032939485f96df595f63082a4bbefa358a7
Mohsen <mohsen.mohammadi.se@gmail.com>
2018-08-14 15:50:25 +0430

    jbuttontutorial

--- src/com/pluralsight/textfile/Gui.java
+++ src/com/pluralsight/textfile/Gui.java
@@ -6,51 +6,71 @@ import java.awt.*;
 public class Gui extends JFrame {
 
     private JTextField tf;
-    private JCheckBox boldBox;
-    private JCheckBox italicBox;
+    private Font pf;
+    private Font bf;
+    private Font itf;
+    private Font bif;
+    private JRadioButton pb;
+    private JRadioButton bb;
+    private JRadioButton ib;
+    private JRadioButton bib;
+    private ButtonGroup group;
 
     public Gui(){
-        super("the title");
+        super("THE TITLE");
         setLayout(new FlowLayout());
 
-        tf = new JTextField("this is a sentence", 20);
-        tf.setFont(new Font("serif", Font.PLAIN, 14 ));
+
+        tf = new JTextField("MOHSEN IS A GREAT GUY!", 26 );
         add(tf);
-        boldBox = new JCheckBox("BOLD");
-        italicBox = new JCheckBox("ITALIC");
-        add(boldBox);
-        add(italicBox);
 
+        pf = new Font("serif", Font.PLAIN, 14);
+        bf = new Font("serif", Font.BOLD, 14);
+        itf = new Font("serif", Font.ITALIC , 14);
+        bif = new Font("serif", Font.BOLD + Font.ITALIC, 14);
 
-        HandlerClass handler = new HandlerClass();
-        boldBox.addItemListener(handler);
-        italicBox.addItemListener(handler);
+        pb = new JRadioButton("plain", true);
+        bb = new JRadioButton("bold", false);
+        ib = new JRadioButton("italic", false);
+        bib = new JRadioButton("bold and italic", false);
+        add(pb);
+        add(bb);
+        add(ib);
+        add(bib);
 
+        group = new ButtonGroup();
+        group.add(pb);
+        group.add(bb);
+        group.add(ib);
+        group.add(bib);
+
+        // wait for event to happen, pass in font object to instructor
+        pb.addItemListener(new HandlerClass(pf));
+        bb.addItemListener(new HandlerClass(bf));
+        ib.addItemListener(new HandlerClass(itf));
+        bib.addItemListener(new HandlerClass(bif));
 
     }
-
     private class HandlerClass implements ItemListener{
-        public void itemStateChanged(ItemEvent event){
-            Font font = null;
-            if(boldBox.isSelected()&& italicBox.isSelected()) {
-                font = new Font("serif", Font.BOLD + Font.ITALIC, 14);
-            }
-                else if(boldBox.isSelected()){
-                    font = new Font("serif", Font.BOLD,14);
+        private Font font;
 
-                }
-                else if (italicBox.isSelected()){
-                font = new Font("serif", Font.ITALIC,14);
-            }
-            else
-                font = new Font("serif", Font.PLAIN, 14);
-            tf.setFont(font);
 
+        // the Font object gets variable font
+        public HandlerClass(Font f){
+            font = f;
         }
+        public void itemStateChanged(ItemEvent event){
+            tf.setFont(font);
+        }
+
+
+
+
     }
 
 
 
 
+    }
 
-        }
+
